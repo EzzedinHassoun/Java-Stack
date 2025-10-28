a07dd56a8c77923ea0dbcbe2ec80f2b6bcd2bc42
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-10-28 18:03:42 +0200

    cafe

--- Cafe Java/CafeJava.java
+++ Cafe Java/CafeJava.java
@@ -1,57 +1,49 @@
 public class CafeJava {
     public static void main(String[] args) {
-        // APP VARIABLES
-        // Lines of text that will appear in the app. 
         String generalGreeting = "Welcome to Cafe Java, ";
         String pendingMessage = ", your order will be ready shortly";
-        String readyMessage = ", your order is ready";
+        String readyMessage = ", your order is ready. ";
         String displayTotalMessage = "Your total is $";
 
-        // Menu variables (add yours below)
         double mochaPrice = 3.5;
-        int cocktail = 30;
-        int milkShake = 20;
+        int dripCoffee = 30;
+        int latte = 50;
         int cappuccino = 25;
-        // Customer name variables (add yours below)
-        String customer1 = "Ezzedin";
-        String customer2 = "Mahmoud";
-        String customer3 = "Sudqi";
-        String customer4 = "Hassoun";
-        String customer5 = "Ahmad";
 
-        
-        // Order completions (add yours below)
+        String customer1 = "Shatha";
+        String customer2 = "Ahmad";
+        String customer3 = "Sali";
+        String customer4 = "Adam";
+
+        boolean isReadyOrderAdam = false;
+        boolean isReadyOrderSali = true;
         boolean isReadyOrderAhmad = true;
-        boolean isReadyOrderSudqi = true;
-        boolean isReadyOrderMahmoud = true;
-        boolean isReadyOrderHassoun = true;
-        boolean isReadyOrderEzzedin = true;
-                            
+        boolean isReadyOrderShatha = false;
+
         System.out.println(generalGreeting + customer1);
-        if (isReadyOrderHassoun){
-            System.out.println(customer4 + readyMessage + displayTotalMessage +""+cappuccino );
-        }else{
+
+        if (isReadyOrderAdam){
+            System.out.println(customer4 + readyMessage + displayTotalMessage + ""+cappuccino);
+        } else {
             System.out.println(customer4 + pendingMessage);
         }
-        if (isReadyOrderMahmoud){
-            System.out.println(customer2 + readyMessage + displayTotalMessage +""+milkShake );
-        }else{
+
+        if (isReadyOrderAhmad){
+            System.out.println(customer2 + readyMessage + displayTotalMessage + ""+latte);
+        } else {
             System.out.println(customer2 + pendingMessage);
         }
-        if (isReadyOrderSudqi){
-            System.out.println(customer3 + readyMessage + displayTotalMessage +""+(mochaPrice + milkShake));
-        }else{
-            System.out.println(customer3 + pendingMessage);
-        }
-        if (isReadyOrderEzzedin){
-            System.out.println(customer1 + readyMessage + displayTotalMessage +""+cocktail );
-        }else{
+
+        if (isReadyOrderShatha){
+            System.out.println(customer1 + readyMessage + displayTotalMessage + ""+dripCoffee);
+        } else {
             System.out.println(customer1 + pendingMessage);
         }
-        if (isReadyOrderAhmad){
-            System.out.println(customer5 + readyMessage + displayTotalMessage +""+(mochaPrice+cappuccino));
-        }else{
-            System.out.println(customer5 + pendingMessage);
+
+        if (isReadyOrderSali){
+            System.out.println(customer3 + readyMessage + displayTotalMessage + ""+(mochaPrice + cappuccino));
+        } else {
+            System.out.println(customer3 + pendingMessage);
         }
     }
 }

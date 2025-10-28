d8603c916b0bcdad792bdc228cf04fad43bbc226
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-10-28 15:04:15 +0200

    up

---
+++ First Java Program/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava  {
+    public static void main(String[] args){
+        System.out.println("My Name Is Ezzedin");
+        System.out.println("i am 31 years old");
+        System.out.println("I live in Nablus");
+}
+}

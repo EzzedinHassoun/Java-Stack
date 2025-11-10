2016a5612bc8738242fa3518012d0b6278d796ad
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-11-10 19:44:29 +0200

    up

---
+++ FruitsLoops/src/test/java/com/Fruits/Loops/FruitsLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fruits.Loops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

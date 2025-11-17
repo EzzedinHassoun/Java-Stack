6d14d20c882dd59b46f858506d02fe13a6af4475
Ezzedin M.S. Hassoun <ezzedinhassoun_1994@hotmail.com>
2025-11-17 16:37:56 +0200

    up

---
+++ Dojo_and_Ninjas/src/main/java/com/codingdojo/demo/repositories/NinjaRepositories.java
@@ -0,0 +1,12 @@
+package com.codingdojo.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codingdojo.demo.models.Ninja;
+
+public interface NinjaRepositories extends CrudRepository<Ninja, Long>{
+    List<Ninja> findAll();
+
+}
